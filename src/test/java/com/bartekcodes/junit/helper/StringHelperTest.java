b348907f1f6fea81165d06e3b8b113fa5befdd3e
Bartek <bl4kee@wp.pl>
2021-03-16 10:51:28 +0100

    Initial commit

---
+++ src/test/java/com/bartekcodes/junit/helper/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.bartekcodes.junit.helper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringHelperTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
